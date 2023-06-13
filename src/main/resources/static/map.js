8eed2888819f42869ee0697e7543ca2e2929e219
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2023-06-13 03:26:13 +0300

    Added Cyr

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -1,5 +1,5 @@
 var map = L.map('map').setView([45.528, 45.528], 2);
-L.tileLayer('https://c.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map',
 	maxZoom: 18,
 	id: 'your.project.id',
