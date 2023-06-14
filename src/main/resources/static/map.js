c056377d7b50fc366ffca59ea8bee34605637320
Емельянов Константин Валентинович <emelyanovkonstantin@yandex.ru>
2023-06-15 00:30:49 +0300

    Added med_murmansk.geojson

--- src/main/resources/static/map.js
+++ src/main/resources/static/map.js
@@ -4,3 +4,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	id: 'your.project.id',
 }).addTo(map);
+
+var geojsonMarkerOptions = {
+    radius: 8,
+    fillColor: "#ff7800",
+    color: "#000",
+    weight: 1,
+    opacity: 1,
+    fillOpacity: 0.8
+};
+ function forEachFeature(feature, layer) {
+        console.log(feature.properties);
+        var popupContent = "<p> <b>" + JSON.stringify(feature.properties) + "</p>";
+
+        layer.bindPopup(popupContent);
+}
+
+var medMurmanskData = new L.GeoJSON.AJAX("http://localhost:8080/data/med_murmansk.geojson",{
+    onEachFeature: forEachFeature,
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+}).addTo(map);
