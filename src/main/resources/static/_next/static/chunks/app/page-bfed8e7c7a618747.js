dde850797b39fee6a574d87d7885388644fb9520
Kita Biserov <72317821+KitaBiserov@users.noreply.github.com>
2023-07-25 21:19:37 +0300

    Static Update

---
+++ src/main/resources/static/_next/static/chunks/app/page-bfed8e7c7a618747.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,160],{5983:function(n,e,u){Promise.resolve().then(u.t.bind(u,4424,23))}},function(n){n.O(0,[424,580,502,744],function(){return n(n.s=5983)}),_N_E=n.O()}]);
