745e815e009d45800ce8e42e208cc596a9457836
Nikita Biserov <72317821+KitaBiserov@users.noreply.github.com>
2023-08-05 14:59:03 +0300

    Update Static
    Card Refactoring, NoTileLayer, Create Route by City Id

---
+++ src/main/resources/static/_next/static/chunks/app/dashboard/analytics/page-67e25286d57fee4c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[171],{6951:function(e,n,s){Promise.resolve().then(s.t.bind(s,4656,23)),Promise.resolve().then(s.t.bind(s,4904,23)),Promise.resolve().then(s.bind(s,9151)),Promise.resolve().then(s.bind(s,2750))}},function(e){e.O(0,[929,121,179,690,744],function(){return e(e.s=6951)}),_N_E=e.O()}]);
